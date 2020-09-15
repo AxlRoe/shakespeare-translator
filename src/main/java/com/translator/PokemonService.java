e4705ccec761581c389a07eb579ddddc0dd08ff0
mmarini <Matteo_Marini@bmc.com>
2020-09-15 18:35:28 +0200

    s user I want to retrieve pokemon's description

---
+++ src/main/java/com/translator/PokemonService.java
@@ -0,0 +1,7 @@
+package com.translator;
+
+public class PokemonService {
+    public String getPokemonDescription(String pokemonId) {
+        return "My amazing pokemon";
+    }
+}
---
+++ src/test/java/com/translator/PokemonServiceUnitTest.java
@@ -0,0 +1,24 @@
+package com.translator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PokemonServiceUnitTest {
+
+    PokemonService service;
+
+    @BeforeEach
+    void setUp () {
+        service = new PokemonService();
+    }
+
+    @Test
+    @DisplayName("As user I want to retrieve pokemon's description")
+    public void givenPokemonName_retrieveItsDescription () {
+        String description = service.getPokemonDescription("foo");
+        assertThat(description).isEqualTo("My amazing pokemon");
+    }
+
+}
